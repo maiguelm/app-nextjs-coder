ced1b47ab8016bb04d89b1eb566dfe50710bc9ae
maiguelm <migmoyano86@gmail.com>
2024-03-02 11:52:36 -0300

    nuevo productsAdmin

--- components/admin/ProductsAdmin.js
+++ components/admin/ProductsAdmin.js
@@ -1,4 +1,4 @@
-import Image from "next/image";
+/* import Image from "next/image";
 import Link from "next/link";
 
 
@@ -84,4 +84,104 @@ const ProductsAdmin = async () => {
 };
 
 export default ProductsAdmin;
- 
+  */
+
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "@/firebase/config";
+import Image from "next/image";
+import Link from "next/link";
+
+const ProductsAdmin = ({ products }) => {
+    return (
+        <>
+            <h1>Lista de Productos</h1>
+            <div className="overflow-x-auto">
+                <table className="w-full text-xs text-left text-gray-600">
+                    <thead className="text-xs text-gray-700 uppercase">
+                        <tr>
+                            <th scope="col" className="px-3 py-2">
+                                Nombre
+                            </th>
+                            <th scope="col" className="px-3 py-2">
+                                Precio
+                            </th>
+                            <th scope="col" className="px-3 py-2">
+                                En stock
+                            </th>
+                            <th scope="col" className="px-3 py-2">
+                                Tipo
+                            </th>
+                            <th scope="col" className="px-3 py-2">
+                                Imagen
+                            </th>
+                            <th scope="col" className="px-3 py-2">
+                                Slug
+                            </th>
+                            <th scope="col" className="px-3 py-2">
+                                Descripción
+                            </th>
+                            <th scope="col" className="px-3 py-2">
+                                Acciones
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {products.map((item) => (
+                            <tr key={item.slug}>
+                                <td className="p-2">{item.nombre}</td>
+                                <td className="p-2">{item.precio}</td>
+                                <td className="p-2">{item.cantidad}</td>
+                                <td className="p-2">{item.categoria}</td>
+                                <td className="p-2">
+                                    <Image
+                                        src={item.imagen}
+                                        alt={item.nombre}
+                                        width={80}
+                                        height={80}
+                                    />
+                                </td>
+                                <td className="p-2">{item.slug}</td>
+                                <td className="p-2 truncate max-w-prose">{item.descripcion}</td>
+                                <td className="p-2">
+                                    <Link
+                                        href={`/admin/edit/${item.slug}`}
+                                        className="rounded bg-green-400 p-2 text-white"
+                                    >
+                                        Editar
+                                    </Link>
+                                    <Link
+                                        href={`/admin/delete/${item.slug}`}
+                                        className="rounded bg-red-400 p-2 text-white"
+                                    >
+                                        Eliminar
+                                    </Link>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </>
+    );
+};
+
+export async function getServerSideProps() {
+    try {
+        const querySnapshot = await getDocs(collection(db, "products"));
+        const productsData = querySnapshot.docs.map(doc => ({ ...doc.data(), id: doc.id }));
+        return {
+            props: {
+                products: productsData
+            }
+        };
+    } catch (error) {
+        console.error("Error al obtener productos:", error);
+        return {
+            props: {
+                products: []
+            }
+        };
+    }
+}
+
+export default ProductsAdmin;
