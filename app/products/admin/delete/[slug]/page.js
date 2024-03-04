d6a989e2c66cde10fdd67f1cd1318083b6df7c87
maiguelm <migmoyano86@gmail.com>
2024-03-04 19:57:07 -0300

    correccion api edit y delet product

--- app/products/admin/delete/[slug]/page.js
+++ app/products/admin/delete/[slug]/page.js
@@ -3,10 +3,12 @@ import DeleteForm from "@/components/admin/DeleteForm"
 
 const EditPage = async ({params}) => {
     const { slug } = params
-   const item = await fetch(`${process.env.VERCEL_URL}/products/product/${slug}`, {
-        cache: 'no-store'
-    }).then(res => res.json())
+    const apiUrl = "https://app-nextjs-coder.vercel.app/api"; 
+    const item = await fetch(`${apiUrl}/products/product/${slug}`, {
+      cache: "no-store",
+    }).then((r) => r.json());
 
+   
     console.log(slug)
 
     return (
